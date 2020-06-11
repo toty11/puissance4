87994cdf9205cbe3ab5e2de405d86f2124fb36f7
toty11 <xtoty971@gmail.com>
2020-06-10 20:33:45 -0400

    v1
    Jeu ok mais problème de stockage du gagnant car l'api change de valeur trop vite
    Amélioration interface

--- src/app/plateau/plateau.component.ts
+++ src/app/plateau/plateau.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Subscription, Observable, timer } from 'rxjs';
 
 @Component({
   selector: 'app-plateau',
@@ -15,88 +16,77 @@ export class PlateauComponent implements OnInit {
   ligne_quatre = [];
   ligne_cinq = [];
   ligne_six = [];
-  user = {identifiant: ''};
+  user = {identifiant: '', pseudo: ''};
   colonne = new FormControl('');
   statut_info = "";
   adversaire = "";
   etat_partie = "";
+  display_plateau = false;
   tour = "";
   joueur = "";
   winner = "";
+  tourInfo = "";
+  subscription: Subscription;
 
   constructor() { }
 
+  everySecond: Observable<number> = timer(1000,2000);
+  
   ngOnInit(): void {
     this.initUser();
-    this.initPlateau();
     this.statut();
-    //lancer timeout après participation si pas d'adversaire
-    //lancer timeout si partie en cours
-    setTimeout(() => {
-      this.initPlateau();
-    },1000);
+    this.initPlateau();
   }
 
   //Initialise la variable user avec l'user connecté
   initUser(): void{
     var users = JSON.parse(localStorage.getItem('users'));
     users.forEach((u) => {if(u.currentUser == "1"){ this.user = u;}});
-    console.log(this.user);
   }
 
   initPlateau(): void{
-    console.log(this.user.identifiant);
     const url = "https://trankillprojets.fr/P4/?statut&identifiant="+this.user.identifiant;
 
     var self = this;
     fetch(url)
     .then((response) => response.json())
     .then(function(data) {
-      console.log(data);
-        if(data.carte != "undefined"){
-          self.joueur = data.joueur;
-          var tab = data.carte;
-          self.ligne_six = tab.substring(2,15).split(",","7");
-          self.ligne_cinq = tab.substring(18,31).split(",","7");
-          self.ligne_quatre = tab.substring(34,47).split(",","7");
-          self.ligne_trois = tab.substring(50,63).split(",","7");
-          self.ligne_deux = tab.substring(66,79).split(",","7");
-          self.ligne_un = tab.substring(82,95).split(",","7");
-          self.tab_plateau = [self.ligne_six,self.ligne_cinq,self.ligne_quatre,self.ligne_trois,self.ligne_deux,self.ligne_un];
-
-          if(data.statut.search("gagne") == -1){
-            self.tab_rempli = true;
-          }
-          /*
-          console.log(self.ligne_six);
-          console.log(self.ligne_cinq);
-          console.log(self.ligne_quatre);
-          console.log(self.ligne_trois);
-          console.log(self.ligne_deux);
-          console.log(self.ligne_un); */
-        }else{
-          self.etat_partie = "Aucune partie en cours";
+      if(typeof(data.carte) !== 'undefined'){
+        self.joueur = data.joueur;
+        let tab = JSON.stringify(data.carte);
+      
+        //Création de tableau de dimension 7 de chaque ligne
+        //Permet l'affichage du plateau avec ngFor
+        self.ligne_six = tab.substring(2,15).split(",",7);
+        self.ligne_cinq = tab.substring(18,31).split(",",7);
+        self.ligne_quatre = tab.substring(34,47).split(",",7);
+        self.ligne_trois = tab.substring(50,63).split(",",7);
+        self.ligne_deux = tab.substring(66,79).split(",",7);
+        self.ligne_un = tab.substring(82,95).split(",",7);
+        self.tab_plateau = [self.ligne_six,self.ligne_cinq,self.ligne_quatre,self.ligne_trois,self.ligne_deux,self.ligne_un];
+        if(data.etat.search("gagne") == -1){
+          self.tab_rempli = true;
         }
+      }
     });
   }
 
-  
-
-  jouer(): void{
-    const url = "https://trankillprojets.fr/P4/?jouer&position="+this.colonne.value+"&identifiant="+this.user.identifiant;
-
-    var self = this;
-    fetch(url)
-    .then((response) => response.json())
-    .then(function(data) {
-      if(data.joueur == data.tour){
-        self.tour = "C'est votre tour";
-      }else{
-        self.tour = "C'est le tour de votre adversaire";
-      }
-      self.initPlateau();
-      self.statut();
-    });
+  jouer(colonne: number): void{
+    if(this.tour != "C'est le tour de votre adversaire"){
+      const url = "https://trankillprojets.fr/P4/?jouer&position="+colonne+"&identifiant="+this.user.identifiant;
+      
+      var self = this;
+      fetch(url)
+      .then((response) => response.json())
+      .then(function(data) {
+        console.log("JOUER");
+        console.log(data);
+        self.initPlateau();
+        self.statut();
+      });
+    }else{
+      alert("Ce n'est pas votre tour");
+    }
   }
 
   participer(): void{
@@ -106,8 +96,10 @@ export class PlateauComponent implements OnInit {
     fetch(url)
     .then((response) => response.json())
     .then(function(data) {
-      console.log("PARTICIPER");
-      console.log(data);
+      self.subscription = self.everySecond.subscribe((seconds) => {
+        self.initPlateau();
+        self.statut();
+      });
     });
   }
 
@@ -120,34 +112,64 @@ export class PlateauComponent implements OnInit {
     .then(function(data) {
       console.log("STATUT");
       console.log(data);
-      if(data.statut != "en cours"){
-        self.etat_partie = "Aucune partie en cours";
-        self.joueur = "";
-        if(data.statut.search("gagne")){
-          if(data.statut.search(data.joueur) != -1){
-            self.winner = "Vous avez gagné votre dernière partie contre "+data.adversaire;
-          }else{
-            self.winner = "Vous avez perdu votre dernière partie contre "+data.adversaire;
-          }
+      
+      if(typeof(data.carte) == 'undefined'){
+        self.display_plateau = false;
+      }
+
+      if(data.etat == "En cours"){
+        if(self.display_plateau == false){
+          self.initPlateau();
+          self.display_plateau = true;
+          self.subscription = self.everySecond.subscribe((seconds) => {
+            self.initPlateau();
+            self.statut();
+          });
         }
-      }else if(data.statut.search("gagne") != -1){
-        if(data.statut.search(data.joueur) != -1){
+        self.etat_partie = "Partie: en cours";
+        self.adversaire = "Adversaire: "+data.adversaire;
+        self.joueur = data.joueur;
+        self.tour = data.tour;
+        if(data.joueur == data.tour){
+          self.tourInfo = "C'est votre tour";
+        }else{
+          self.tourInfo = "C'est le tour de votre adversaire";
+        }
+      }else if(data.etat == "En attente"){
+        self.etat_partie = data.etat+" d'un adversaire";
+      }else if(data.etat.search("gagne") != -1){
+        this.subscription.unsubscribe();
+        if(data.etat.search(data.joueur) != -1){
           self.etat_partie = "Aucune partie en cours";
-          self.winner = "Vous avez gagné contre "+data.adversaire;
+          self.winner = "Vous avez gagné";
           self.joueur = "";
           self.tab_rempli = false;
         }else{
-          self.winner = "Vous avez perdu contre "+data.adversaire;
+          self.winner = "Vous avez perdu contre";
         }
       }
+      else if(data.etat == "Match nul"){
+        self.etat_partie = data.etat;
+        self.joueur = data.joueur;
+        self.adversaire = data.adversaire;
+        self.winner = "Auncun gagnant";
+        self.tab_rempli = false;
+        this.subscription.unsubscribe();
+      }else if(data.etat == "OK"){
+        self.etat_partie = "Aucune partie en cours";
+        self.adversaire = "";
+        self.tourInfo = "";
+        self.joueur = "";
+        self.tab_rempli = false;
+        self.display_plateau = false;
+        this.subscription.unsubscribe();
+      }else if(data.etat == "KO"){
+        self.etat_partie = "Votre compte n'as pas été reconnu.";
+        self.tab_rempli = false;
+        this.subscription.unsubscribe();
+      }
       else{
-        self.adversaire = "Votre adversaire: "+ ((data.adversaire == null)? 'En attente' : data.adversaire);
-        self.etat_partie = "Partie: "+data.statut;
-        if(data.joueur == data.tour){
-          self.tour = "C'est votre tour";
-        }else{
-          self.tour = "C'est le tour de votre adversaire";
-        }
+        self.etat_partie = "Une erreur est survenu, veuillez réessayer";
       }
     });
   }
