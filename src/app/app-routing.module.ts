0050eaf5900416f9ac891f1822de43d9afc30a7a
toty11 <xtoty971@gmail.com>
2020-06-17 15:47:58 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListeJoueurComponent } from './liste-joueur/liste-joueur.component';
+
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { PlateauComponent } from './plateau/plateau.component';
